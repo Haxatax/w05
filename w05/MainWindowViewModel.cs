263b796ff9dc211e80eff46a4242630fa51fb424
halacsygergely <halacsygergely@stud.uni-obuda.hu>
2023-04-18 13:50:43 +0200

    RestCollection

--- w05/MainWindowViewModel.cs
+++ w05/MainWindowViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using ChatBackEnd;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace w05
 {
     internal class MainWindowViewModel
     {
+        public RestCollection<Chatmessage> Messages { get; set; }
 
+        public MainWindowViewModel()
+        {
+            Messages = new RestCollection<Chatmessage>("https://localhost:7233/","message");
+        }
     }
 }
