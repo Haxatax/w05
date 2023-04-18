4c32809f87d8475c4c89b8b355ea58d43f356a3c
halacsygergely <halacsygergely@stud.uni-obuda.hu>
2023-04-18 13:33:00 +0200

    Chatmessage.cs valamint a controllerre

---
+++ ChatBackEnd/Chatmessage.cs
@@ -0,0 +1,22 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ChatBackEnd
+{
+    public class Chatmessage
+    {
+        string datamessage;
+        string name;
+        DateTime sendtime;
+
+        public DateTime Sendtime { get ; set; }
+        public string Name { get; set; }
+        public string Datamessage { get; set; }
+
+        public Chatmessage(string Datamessage, string Name)
+        {
+            this.Datamessage = Datamessage;
+            this.Name = Name;
+            Sendtime = DateTime.Now;
+        }
+    }
+}
---
+++ ChatBackEnd/Controllers/ChatmessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatmessageController : ControllerBase
+    {
+        private static List<Chatmessage> messages = new List<Chatmessage>();
+
+        [HttpPost]
+        public IActionResult AddMessage(Chatmessage message)
+        {
+            messages.Add(message);
+            return Ok();
+        }
+        [HttpGet]
+        public List<Chatmessage> GetMessages()
+        {
+            return messages;
+        }
+
+    }
+}
